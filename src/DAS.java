30b8f14f6f37d4cfead2e7a009221ed7c4e58162
Wiktor <wiko700@gmail.com>
2024-11-21 19:52:25 +0100

    feat: add custom logs and possibility to run in slave mode

--- src/DAS.java
+++ src/DAS.java
@@ -3,9 +3,10 @@ import java.net.SocketException;
 
 public class DAS {
     public static void main(String[] args) {
+        Log.PREFIX = "D";
 
         if (args.length != 2) {
-            System.out.println("Usage: java DAS <port> <number>");
+            Log.log("Usage: java DAS <port> <number>");
             return;
         }
 
@@ -16,41 +17,35 @@ public class DAS {
             port = Integer.parseInt(args[0]);
             number = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            System.out.println("Error: Both arguments must be integers.");
+            Log.log("Error: Both arguments must be integers.");
             return;
         }
-
-        System.out.println("Starting UDP server");
-
-        System.out.println("Opening UDP socket");
 
         if (isMaster(port)) {
             runAsMaster(port, number);
             return;
         }
 
-        runAsSlave(port);
+        runAsSlave(port, number);
     }
 
     private static boolean isMaster(int port) {
         try (DatagramSocket socket = new DatagramSocket(port)) {
-            System.out.println("Port " + port + " is available. Running as master.");
+            Log.log("Port " + port + " is available. Running as master.");
             return true;
         } catch (SocketException e) {
-            System.out.println("Port " + port + " is already in use. Running as slave.");
+            Log.log("Port " + port + " is already in use. Running as slave.");
             return false;
         }
     }
 
     private static void runAsMaster(int port, int number) {
-
-        System.out.println("Starting in master mode...");
-
         MasterMode masterMode = new MasterMode(port, number);
         masterMode.start();
     }
 
-    private static void runAsSlave(int port) {
-        System.out.println("Starting in slave mode...");
+    private static void runAsSlave(int port, int number) {
+        SlaveMode slaveMode = new SlaveMode(port, number);
+        slaveMode.start();
     }
 }
